daa045b7451fdf624962a4f9e4a07ebb0dd20b16
matin0728 <matin0728@gmail.com>
2012-11-21 19:42:44 +0800

    update comments list.

--- shared/app_config.py
+++ shared/app_config.py
@@ -5,5 +5,5 @@ debug = os.environ.get('SERVER_SOFTWARE', '').startswith('Dev')
 
 SITE_CONFIG = {
   'is_debug': debug,
-  'static_file_version': '1211211737236348'
+  'static_file_version': '1211211855758945'
 }
