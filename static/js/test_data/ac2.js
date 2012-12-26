5c734ada466d508b0643046b104cdd0cd16fbac4
matin0728 <matin0728@gmail.com>
2012-12-26 17:13:49 +0800

    update test data.

--- static/js/test_data/ac2.js
+++ static/js/test_data/ac2.js
@@ -11,7 +11,7 @@
             'url_token': '456',
             'avatar': 'http://p2.zhimg.com/b5/42/b542a673c_m.jpg'
         },
-        'html':'<div class="ac-row"><a class="item-topic">知乎</a><span class="muted">2733 个热门回答</span></div>'
+        'html':'<div class="ac-row"><a class="item-topic" href="http://www.zhihu.com/topic/19550228">知乎</a><span class="muted">2733 个热门回答</span></div>'
     },
     {
         'entity':{
@@ -21,7 +21,7 @@
             'url_token': '456',
             'avatar': 'http://p2.zhimg.com/b5/42/b542a673c_m.jpg'
         },
-        'html':'<div class="ac-row"><a class="item-topic">知乎</a><span class="muted">2733 个热门回答</span></div>'
+        'html':'<div class="ac-row"><a class="item-topic" href="http://www.zhihu.com/topic/19550228">知乎</a><span class="muted">2733 个热门回答</span></div>'
     },
     {'unselectable':1, 'html':'<div class="ac-row-label">用户</div>'},
     {
@@ -33,7 +33,7 @@
             'url_token': '456',
             'avatar': 'http://p2.zhimg.com/b5/42/b542a673c_m.jpg'
         },
-        'html':'<div class="ac-row"><a class="member-link"><img src="http://p2.zhimg.com/f8/5c/f85cbedfc_s.jpg" class="avatar avatar-small" /><span class="user-info"><strong>知心</strong><small>这个网站的傻超越我的想象，但祝你们成功。</small></span></a></div>'
+        'html':'<div class="ac-row"><a class="member-link" href="http://www.zhihu.com/people/loki-tang"><img src="http://p2.zhimg.com/f8/5c/f85cbedfc_s.jpg" class="avatar avatar-small" /><span class="user-info"><strong>知心</strong><small>这个网站的傻超越我的想象，但祝你们成功。</small></span></a></div>'
     },
     {
         'entity':{
@@ -43,7 +43,7 @@
             'url_token': '456',
             'avatar': 'http://p2.zhimg.com/b5/42/b542a673c_m.jpg'
         },
-        'html':'<div class="ac-row"><a class="member-link"><img src="http://p2.zhimg.com/f8/5c/f85cbedfc_s.jpg" class="avatar avatar-small" /><span class="user-info"><strong>知心</strong><small>这个网站的傻超越我的想象，但祝你们成功。</small></span></a></div>'
+        'html':'<div class="ac-row"><a class="member-link" href="http://www.zhihu.com/people/loki-tang"><img src="http://p2.zhimg.com/f8/5c/f85cbedfc_s.jpg" class="avatar avatar-small" /><span class="user-info"><strong>知心</strong><small>这个网站的傻超越我的想象，但祝你们成功。</small></span></a></div>'
     },
     {'unselectable':1, 'html':'<div class="ac-row-label">问题</div>'},
     {
@@ -53,7 +53,7 @@
             'title': '有哪些值得推荐的微信公众帐号？它们各有哪些特色？',
             'url_token': '456'
         },
-        'html':'<div class="ac-row"><i class="icon-start"></i><a class="question-link">有哪些值得推荐的微信公众帐号？它们各有哪些特色？<span class="info">2 个答案</span></a></div>'
+        'html':'<div class="ac-row"><i class="icon-start"></i><a class="question-link" href="http://www.zhihu.com/question/19974418/answer/13528241">有哪些值得推荐的微信公众帐号？它们各有哪些特色？<span class="info">2 个答案</span></a></div>'
     },
     {
         'entity':{
@@ -62,6 +62,6 @@
             'title': '有哪些值得推荐的微信公众帐号？它们各有哪些特色？',
             'url_token': '456'
         },
-        'html':'<div class="ac-row"><a class="question-link">有哪些值得推荐的微信公众帐号？它们各有哪些特色？<span class="info">2 个答案</span></a></div>'
+        'html':'<div class="ac-row"><a class="question-link" href="http://www.zhihu.com/question/19974418/answer/13528241">有哪些值得推荐的微信公众帐号？它们各有哪些特色？<span class="info">2 个答案</span></a></div>'
     }
 ]
