2e154ba035dc4425740f69bd3ec186709cd688ff
matin0728 <matin0728@gmail.com>
2013-10-26 12:38:19 +0800

    update test node.

--- nodes/test_node.py
+++ nodes/test_node.py
@@ -3,14 +3,18 @@
 from shared import node
 
 class TestNode2(node.ZNode):
-	# constructor_name = 'TestNode2'
-	js_path = 'test_node_2'
-	template = 'test_node_2.html'
+	def __init__(self, meta = {}, parent_node = None):
+		super(TestNode2, self).__init__(meta = meta, parent_node = parent_node)
+		self.js_path = 'test_node_2'
+		self.template = 'test_node_2.html'
+		
 
 class TestNode(node.ZNode):
-	# constructor_name = 'TestNode'
-	js_path = 'test_node'
-	template = 'test_node.html'
+	def __init__(self, meta = {}, parent_node = None):
+		super(TestNode, self).__init__(meta = meta, parent_node = parent_node)
+		self.js_path = 'test_node'
+		self.template = 'test_node.html'
+		
 
 	def fetch_data(self):
 		self.set_view_data_item('render_test_node_2', self.render_test_node_2())
